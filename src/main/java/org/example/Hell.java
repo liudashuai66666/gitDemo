38b3c6f2973e022ff03056f0c8c62b9390a53fde
liudashuai66666 <2385272606@qq.com>
2024-01-22 14:45:36 +0800

    初始化

---
+++ src/main/java/org/example/Hell.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * @author 23852
+ * @program gitDemo
+ * @description 欢迎
+ * @create 2024-01-22 14:44
+ **/
+public class Hell {
+    public static void main(String[] args) {
+        System.out.println("hell!");
+    }
+}
---
+++ src/main/java/org/example/Play.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * @author 23852
+ * @program gitDemo
+ * @description 玩
+ * @create 2024-01-22 14:45
+ **/
+public class Play {
+    public static void main(String[] args) {
+        System.out.println("play games!");
+    }
+}
